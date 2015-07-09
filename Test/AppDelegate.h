f2b975b4122d61a50843d7083baf2810d480719f
Nina Lu <ninajlu@gmail.com>
2015-07-09 15:46:16 -0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Nina Lu on 7/9/15.
+//  Copyright (c) 2015 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Test
+//
+//  Created by Nina Lu on 7/9/15.
+//  Copyright (c) 2015 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
